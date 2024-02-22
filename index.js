1dd1cd2058191333fe0b76562d171e17a423af2b
Francisco Meza Dastres <franciscomeza.1234@gmail.com>
2024-02-21 22:13:07 -0300

    reducing codding review

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const {registrar,leer} = require('./operaciones.js');
-const operacion = process.argv[2];
-const nombre = process.argv[3];
-const edad = process.argv[4];
-const tipo = process.argv[5];
-const color = process.argv[6];
-const enfermedad = process.argv[7];
-
+// const operacion = process.argv[2];
+// const nombre = process.argv[3];
+// const edad = process.argv[4];
+// const tipo = process.argv[5];
+// const color = process.argv[6];
+// const enfermedad = process.argv[7];
+const [operacion, nombre, edad, tipo, color, enfermedad] = process.argv.slice(2)
 if(operacion === "registrar"){
     registrar(nombre, edad, tipo, color, enfermedad)
     console.log('registro exitoso')
